42010f0ac4eeb74e4e94a0829943f08cbcb17975
Arnis Stasko <arnis.stasko@gmail.com>
2013-07-11 13:29:02 -0700

    Added index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var data = fs.readFileSync('index.html');
-  response.send(data);
+  response.send(data.toString());
 //  response.send('Hello World 2!');
 });
 
